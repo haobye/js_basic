5c8c8b73ae384dff38eb5a106af3e8ea69a0d5b1
hao难懂 <46086267+haobye@users.noreply.github.com>
2020-03-23 09:54:04 +0800

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,10 @@
+
+$(function(){
+	$("div img").addClass("alpha");   
+	$("div img:eq(0)").removeClass("alpha");
+	$("div img").mouseover(function(){
+		var big_src = $(this).attr("src");
+	$("#test").attr("src", big_src);
+		$(this).removeClass("alpha").siblings().addClass("alpha");
+	});
+});
